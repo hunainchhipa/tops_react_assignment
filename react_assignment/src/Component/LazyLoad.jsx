c1d3d4abdb03b530b4c9593db84d80b49d955827
hunainchhipa <hunain.chhipa23@gmail.com>
2023-06-06 23:47:48 +0530

    Lazy Loading

--- react_assignment/src/Component/LazyLoad.jsx
+++ react_assignment/src/Component/LazyLoad.jsx
@@ -4,7 +4,7 @@ export default function LazyLoad() {
   return (
     <div>
       <h1 className="position-absolute top-50 start-50 translate-middle">
-        This is the Example of Lazy Loading.
+        This is an Example of Lazy Loading.
       </h1>
     </div>
   );
